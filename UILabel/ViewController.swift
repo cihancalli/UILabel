918cd51f87c566ea3290f7dbf0885ad70879da89
cihancalli <cihan.callii@gmail.com>
2022-03-14 23:33:45 +0300

    Changing UI Label text from View Controller

--- UILabel/ViewController.swift
+++ UILabel/ViewController.swift
@@ -9,9 +9,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        label.text = "Merhaba, IOS!"
+        label.font = UIFont.systemFont(ofSize: 17.0)
+        label.textColor = UIColor.red
+        
     }
 
 
